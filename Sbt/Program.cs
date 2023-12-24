9c677bd6d1f36219859198cfce714077711dcaed
jzuras <jim.zuras@gmail.com>
2023-12-24 17:50:41 -0500

    Disabling this on Azure as it has become OBE.(forgot 1 file)

--- Sbt/Program.cs
+++ Sbt/Program.cs
@@ -12,12 +12,17 @@ namespace Sbt
             // Add services to the container.
             builder.Services.AddRazorPages();
 
-            builder.Services.AddDbContext<DemoContext>(options =>
-                options.UseSqlServer(builder.Configuration.GetConnectionString("AZURE_SQL_CONNECTIONSTRING") ?? throw new InvalidOperationException("Connection string 'DemoContext' not found.")));
+            //builder.Services.AddDbContext<DemoContext>(options =>
+                //options.UseSqlServer(builder.Configuration.GetConnectionString("AZURE_SQL_CONNECTIONSTRING") ?? throw new InvalidOperationException("Connection string 'DemoContext' not found.")));
 
             builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
             var app = builder.Build();
+
+            // As of Dec 2023 I am disabling most of this site, except the home page (it is now OBE).
+            // This middleware allows the home page to still work and all others to be not found,
+            // since i don't want to delete all the pages themselves at this point.
+            app.UseMiddleware<DisablePagesMiddleware>();
 
             // Configure the HTTP request pipeline.
             if (!app.Environment.IsDevelopment())
@@ -31,13 +36,13 @@ namespace Sbt
                 app.UseMigrationsEndPoint();
             }
 
-            using (var scope = app.Services.CreateScope())
-            {
-                var services = scope.ServiceProvider;
+            //using (var scope = app.Services.CreateScope())
+            //{
+            //    var services = scope.ServiceProvider;
 
-                var context = services.GetRequiredService<DemoContext>();
-                context.Database.EnsureCreated();
-            }
+            //    var context = services.GetRequiredService<DemoContext>();
+            //    context.Database.EnsureCreated();
+            //}
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
